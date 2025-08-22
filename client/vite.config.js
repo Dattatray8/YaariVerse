a6bc3bb3d0a88720f785b3ffa7cd34a5c9e37802
Dattatray8 <dattatraymahindrakar@gmail.com>
2025-08-22 07:51:05 +0530

    Created PWA

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,39 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Yaari Verse",
+        short_name: "YV",
+        description: "Share your life",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/icon.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+    }),
+  ],
 });
