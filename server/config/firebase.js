a592fabaeba2f6a5f1c6fda10942a0e5bbd09c25
Dattatray8 <dattatraymahindrakar@gmail.com>
2025-12-13 22:45:05 +0530

    Refactor Firebase service account initialization for cleaner code

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -1,8 +1,6 @@
 import admin from "firebase-admin";
 
-const serviceAccount = JSON.parse(
-  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, "base64").toString("utf-8")
-);
+const serviceAccount = JSON.parse(Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, 'base64').toString('utf-8'));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
