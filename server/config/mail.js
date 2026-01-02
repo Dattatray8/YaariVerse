0d9e574a3c2db44a410873cbc93f2a4d31ca3e1c
Dattatray8 <dattatraymahindrakar@gmail.com>
2026-01-02 18:09:32 +0530

    Refactor email sending functionality to use SendGrid and update dependencies

--- server/config/mail.js
+++ server/config/mail.js
@@ -1,24 +1,34 @@
-import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 
-const transporter = nodemailer.createTransport({
-  service: "Gmail",
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.EMAIL_PASSWORD,
-  },
-});
+import sgMail from "@sendgrid/mail";
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendMail = async (to, otp) => {
-  await transporter.sendMail({
-    from: `${process.env.EMAIL}`,
-    to: to,
-    subject: "Reset Your Password",
-    html: `<p> Your OTP for password reset is <b>${otp}</b>. It expires in 5 minutes.`,
-  });
+  try {
+    await sgMail.send({
+      to,
+      from: process.env.EMAIL,
+      subject: "YaariVerse Password Reset OTP",
+      html: `
+    <p>Hello,</p>
+    <p>Your OTP to reset your YaariVerse password is: <b>${otp}</b>.</p>
+    <p>This OTP expires in 5 minutes.</p>
+    <p>Do not share it with anyone.</p>
+    <p>Regards,<br>YaariVerse Team</p>
+  `,
+      replyTo: process.env.EMAIL,
+      headers: {
+        "X-Priority": "1",
+        "X-MSMail-Priority": "High",
+        Importance: "High",
+      },
+    });
+    console.log("Email sent!");
+  } catch (error) {
+    console.error("SendGrid error:", error);
+  }
 };
 
 export default sendMail;
